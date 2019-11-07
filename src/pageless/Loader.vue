972918fe9812b2cbbd017c037b03f0386e845e74
davidot <david.t@glazenkamp.net>
2019-11-07 20:44:05 +0100

    Change loader colours to be more appropriate
    Also added a spinner to show it is doing things

--- src/pageless/Loader.vue
+++ src/pageless/Loader.vue
@@ -1,21 +1,30 @@
 <template>
     <div class="loader-footer" :style="{'max-height': maxHeight}">
-        {{loadState}}
+        <div class="inner-load">
+            <scale-loader :loading="!done" :color="'white'"
+                          :height="'15px'" class="scale-loader-lib">
+            </scale-loader>
+            {{loadState}}
+        </div>
     </div>
 </template>
 
 <script>
 import {mapState} from 'vuex';
+import ScaleLoader from 'vue-spinner/src/ScaleLoader';
 
 export default {
   name: 'Loader',
+  components: {
+    ScaleLoader,
+  },
   computed: {
     ...mapState({
       loadState: (state) => state.libraries.message,
       done: (state) => state.libraries.done,
     }),
     maxHeight: function() {
-      return this.done ? '0' : '45px';
+      return this.done ? '0' : '55px';
     },
   },
   created() {
@@ -24,16 +33,29 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
     .loader-footer {
         position: absolute;
         bottom: 0;
-        color: red;
-        width: 100%;
-        background-color: purple;
+        left: 5%;
+        color: $color-on-primary;
+        width: 95%;
+        background-color: $color-primary;
         transition: max-height 0.5s;
         transition-delay: 0.5s;
         overflow: hidden;
+    }
+
+    .inner-load {
+        border: 5px solid $color-primary-dark;
+        border-right: 0;
         padding: 0 25px;
+    }
+
+    .scale-loader-lib {
+        height: 20px;
+        margin-top: 5px;
+        margin-right: 1em;
+        display: inline;
     }
 </style>
