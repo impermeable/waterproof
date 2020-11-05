441e664209dccfb41184c2d607526570389a1eaa
davidot <david.tuin@gmail.com>
2020-11-05 16:23:18 +0100

    Remove Vue-Devtools installation
    I do not personally use them and I suspect no one else does either
    In case we want them again look here:
    https://github.com/nklayman/vue-cli-plugin-electron-builder/releases/tag/v2.0.0-rc.4

--- src/background.js
+++ src/background.js
@@ -7,7 +7,6 @@ import {execFile} from 'child_process';
 import path from 'path';
 import {
   createProtocol,
-  installVueDevtools,
 } from 'vue-cli-plugin-electron-builder/lib';
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
@@ -129,14 +128,6 @@ app.on('activate', () => {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', async () => {
-  if (isDevelopment && !process.env.IS_TEST) {
-    // Install Vue Devtools
-    try {
-      await installVueDevtools();
-    } catch (e) {
-      console.error('Vue Devtools failed to install:', e.toString());
-    }
-  }
   createWindow();
 });
 
