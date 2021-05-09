5a1df1d870e7394a398b24621dc5ff0c815a494e
Lulof Pirée <l.l.piree@student.tue.nl>
2021-05-09 09:09:15 +0200

    Create failed attempt at matching a term with a type

---
+++ coq_tactics/coq_ugly_tactic_ports/match_type.v
@@ -0,0 +1,24 @@
+From Ltac2 Require Import Ltac2.
+From Ltac2 Require Option.
+From Ltac2 Require Import Message.
+Require Import Bool.
+
+Definition is_of_type (x:nat) (t:Type) : bool :=
+    match x with
+    | t => true
+    end.
+
+Compute is_of_type 1 bool.
+
+
+
+Ltac2 match_type (x:constr) (t:constr) :=
+    lazy_match! x with
+    | nat => (of_string "x is a nat")
+    | &t => (concat (of_string "x is type ") (of_constr t))
+    | _ => (of_constr x)
+    end.
+
+Goal True.
+    print (match_type (constr:(2)) (constr:(nat))).
+    print (match_type (constr:(1)) (constr:(bool))).
