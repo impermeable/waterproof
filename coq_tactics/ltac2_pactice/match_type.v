caa201e61e7e042e14867734699651dd20c3a4cb
Lulof Pirée <l.l.piree@student.tue.nl>
2021-05-11 11:43:06 +0200

    Use suggestion from forum

--- coq_tactics/ltac2_pactice/match_type.v
+++ coq_tactics/ltac2_pactice/match_type.v
@@ -3,21 +3,16 @@ From Ltac2 Require Option.
 From Ltac2 Require Import Message.
 Require Import Bool.
 
-Definition is_of_type (x:nat) (t:Type) : bool :=
-    match x with
-    | t => true
-    end.
-
-Compute is_of_type 1 bool.
-
+Definition type_of {T : Type} (x : T) := T.
 
 Ltac2 match_type (x:constr) (t:constr) :=
-    lazy_match! x with
-    | nat => (of_string "x is a nat")
-    | context [?z] => (concat (of_string "x is type ") (of_constr z))
-    | _ => (of_constr x)
-    end.
+  let tx:=eval cbv in (type_of $x) in
+  Constr.equal tx t.
+
+Ltac2 Eval match_type constr:(2) constr:(nat).
+Ltac2 Eval match_type constr:(2) constr:(bool).
+Ltac2 Eval match_type constr:(true) constr:(bool).
 
 Goal True.
-    print (match_type (constr:(2)) (constr:(nat))).
-    print (match_type (constr:(1)) (constr:(bool))).
+    print (match_type constr:(0) constr:(bool)).
+    print (match_type constr:(1) constr:(bool)).
