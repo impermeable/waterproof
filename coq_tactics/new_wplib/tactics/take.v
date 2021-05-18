cc8c551cc6db1d8ee450fe758da7db1d2a69e903
Lulof Pirée <l.l.piree@student.tue.nl>
2021-05-18 09:05:34 +0200

    Add licence

--- coq_tactics/new_wplib/tactics/take.v
+++ coq_tactics/new_wplib/tactics/take.v
@@ -7,7 +7,25 @@ Creation date: 17 May 2021
 Version of "Take" tactic that accepts any number of arguments.
 "Take" can be used to eliminate a ∀ clause in the goal,
 by introducing variables.
+
+--------------------------------------------------------------------------------
+
+This file is part of Waterproof-lib.
+
+Waterproof-lib is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Waterproof-lib is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Waterproof-lib.  If not, see <https://www.gnu.org/licenses/>.
 *)
+
 From Ltac2 Require Import Ltac2.
 From Ltac2 Require Option.
 From Ltac2 Require Import Message.
--- coq_tactics/new_wplib/test/test_tactics/take_test.v
+++ coq_tactics/new_wplib/test/test_tactics/take_test.v
@@ -6,13 +6,31 @@ Creation date: 17 May 2021
 
 Testcases for the "Take" tactic.
 Tests pass if they can be run without unhandled errors.
+--------------------------------------------------------------------------------
+
+This file is part of Waterproof-lib.
+
+Waterproof-lib is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Waterproof-lib is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Waterproof-lib.  If not, see <https://www.gnu.org/licenses/>.
 *)
+
 From Ltac2 Require Import Ltac2.
 From Ltac2 Require Option.
 From Ltac2 Require Import Message.
 Add LoadPath "./coq_tactics/new_wplib/" as wplib.
 Load test_auxiliary.
 Load take.
+
 
 (* Test 0: This should work fine *)
 Goal forall n : nat, n <= 2*n.
