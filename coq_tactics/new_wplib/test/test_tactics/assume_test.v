10298b1971686e66cd47b94770903ff40719bcc3
Lulof Pirée <l.l.piree@student.tue.nl>
2021-05-29 09:05:35 +0200

    Update doccomments

--- coq_tactics/new_wplib/test/test_tactics/assume_test.v
+++ coq_tactics/new_wplib/test/test_tactics/assume_test.v
@@ -30,11 +30,12 @@ Load assume.
 
 (*
 --------------------------------------------------------------------------------
-    Testcases for "intro_hyp_from_list".
+*)
+(** * Testcases for [intro_hyp_from_list]
     (These require some auxiliary functions which are defined first)
 *)
 
-(* Subroutine of map_to_second_elem *)
+(** Subroutine of map_to_second_elem *)
 Local Ltac2 rec map_to_second_elem_recursion (x : (ident*constr) list)
                                              (result: (constr list)) :=
     match x with
@@ -46,20 +47,32 @@ Local Ltac2 rec map_to_second_elem_recursion (x : (ident*constr) list)
     | [] => result
     end.
 
-(*
+(** * map_to_second_elem
     Map a list of tuples [(s1, t1), (s2, t2), (s3, t3) ...]
     to a list of the second elements [t1, t2, t3, ...].
 
     Arguments:
-        * x: (ident * constr) list
+        - [x: (ident * constr) list]
     
     Returns:
-        * constr list, second elements of each tuple in x.
+        - [constr list], second elements of each tuple in [x].
 *)
 Ltac2 map_to_second_elem (x : (ident*constr) list) :=
     map_to_second_elem_recursion x [].
 
 
+(** * assert_second_elems_equal
+    Given two [(ident*constr) list], 
+    check if they have the same length,
+    and if at every index, the second element of the tuple at that index
+    is the same for both lists.
+
+    Raises exceptions:
+        - [TestFailedError], if [x] and [y] have a different length.
+        - [TestFailedError], if there exists an [i] 
+                             such that [x[i][1] ≠ y[i][1]].
+
+*)
 Ltac2 assert_second_elems_equal (x : (ident*constr) list) 
                                 (y : (ident*constr) list) :=
     assert_list_equal (map_to_second_elem x) (map_to_second_elem y).
@@ -67,8 +80,9 @@ Ltac2 assert_second_elems_equal (x : (ident*constr) list)
     
     
 
-(* 
-    Test 1: assert the input hypothesis gets renamed 
+(** 
+    * Test 1
+    Assert the input hypothesis gets renamed 
     to the ident given in the matching tuple of the input list. 
 *)
 Goal 0=0 -> 1=1.
@@ -77,44 +91,59 @@ Goal 0=0 -> 1=1.
     assert_hyp_exists @c.
 Abort.
 
-(*
-    Test 2: Assert the matching tuple is removed from the output list.
+(**
+    * Test 2
+    Assert the matching tuple is removed from the output list.
     Case: matching tuple is last.
 *)
 Goal 0=0 -> 1=1.
     intros h.
-    Ltac2 result_t2 := fun () => 
-        intro_hyp_from_list ((@b, constr:(3=3))::(@c, constr:(0=0))::[]) @h.
+    let result :=
+        intro_hyp_from_list ((@b, constr:(3=3))::(@c, constr:(0=0))::[]) @h
+    in
     assert_second_elems_equal (result_t2 ()) ((@b, constr:(3=3))::[]).
 Abort.
 
-(*
-    Test 3: Assert the matching tuple is removed from the output list.
+(**
+    * Test 3
+    Assert the matching tuple is removed from the output list.
     Case: matching tuple is first.
 *)
 Goal 0=0 -> 1=1.
     intros h.
-    Ltac2 result_t3 := fun () => 
-        intro_hyp_from_list ((@b, constr:(0=0))::(@c, constr:(9=9))::[]) @h.
-    assert_second_elems_equal (result_t3 ()) ((@c, constr:(9=9))::[]).
+    let result :=
+        intro_hyp_from_list ((@b, constr:(0=0))::(@c, constr:(9=9))::[]) @h
+    in
+    assert_second_elems_equal result ((@c, constr:(9=9))::[]).
 Abort.
 
-(*
-    Test 4: It is also allowed to use a different name than "h".
+(**
+    * Test 4
+    It is also allowed to use a different name than [h].
 *)
 Goal 0=0 -> 1=1.
     intros m.
     intro_hyp_from_list ((@b, constr:(0=0))::(@c, constr:(9=9))::[]) @m.
 Abort.
 
+(**
+    * Test 5
+    Only 1 item in the list.
+*)
+Goal 0=0 -> 1=1.
+    intros m.
+    let result := intro_hyp_from_list ((@b, constr:(0=0))::[]) @m in
+    assert_second_elems_equal result [].
+Abort.
 
 (*
 --------------------------------------------------------------------------------
-    Testcases for "hyp_is_in_list".
+*) 
+(** * Testcases for [hyp_is_in_list]
 *)
 
-(* 
-    Test 1: the item is in the list, should resolve to true.
+(** * Test 1
+    The item is in the list, should resolve to true.
 *)
 Goal 0=0 -> 1=1.
     intros h.
@@ -124,8 +153,8 @@ Goal 0=0 -> 1=1.
                         ::(@c, constr:(0=0))::[]) @h).
 Abort.
 
-(* 
-    Test 2: the item is NOT in the list, 
+(** * Test 2
+    The item is NOT in the list, 
     should resolve to false.
 *)
 Goal 0=0 -> 1=1.
@@ -136,13 +165,25 @@ Goal 0=0 -> 1=1.
                         ::(@c, constr:(1=1))::[]) @h).
 Abort.
 
-
-(*
---------------------------------------------------------------------------------
-    Testcases for "assume_premise_with_breakdown".
+(** * Test 3
+    Corner case: only one item in the list.
 *)
+Goal 0=0 -> 1=1.
+    intros h.
+        
+    assert_is_true (
+        hyp_is_in_list ((@c, constr:(0=0))::[]) @h).
+Abort.
+
+(* ---------------------------------------------------------------------------*)
+(**
+    * Testcases for [assume_premise_with_breakdown].
+*)
+Ltac2 Eval print(of_string "Testcases for [assume_premise_with_breakdown]").
 
 Goal forall n, n = 1 /\ n = 2 -> False.
     intros n.
-    let inp_list := ((@h0, constr:(n = 1))::(@h1, constr:(n = 1))::[]) in
+    let inp_list := ((@h0, constr:(n = 1))::(@h1, constr:(n = 2))::[]) in
     assume_premise_with_breakdown inp_list.
+Abort.
+
