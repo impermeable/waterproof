a6f01a6ee0f73bee90f4868c334fe499de9ec38e
Cosmin Manea <c.manea@student.tue.nl>
2021-06-03 10:10:06 +0300

    Imporoved Waterprove

--- coq_tactics/new_wplib/waterprove/waterprove.v
+++ coq_tactics/new_wplib/waterprove/waterprove.v
@@ -44,49 +44,10 @@ Require Import Reals.
         * if no proof is found, a message saying this is printed.
 *)
 
-Ltac2 waterprove t s n :=
+Ltac2 waterprove t s (n:int) :=
   first [   solve [auto n using $s with *]
           | solve [eauto n using $s with *]
           | solve [ltac1:(s |- intuition (auto using s with *)) (Ltac1.of_constr s)]
           | solve [ltac1:(s |- intuition (eauto using s with *)) (Ltac1.of_constr s)]
           | print ( concat (of_string "Waterproof could not find a proof of ") (of_constr t) )
         ].
-
-
-Ltac check_status t :=
-    match t with
-        | ( forall _ : ?A, _ ) => idtac "You may need to introduce an arbitrary variable (Take ... : ...) or make an assumption (Assume ... : ...)."
-        | ( exists _ : ?A, _ ) => idtac "You may need to choose a specific value of type " A
-        | _ => idtac
-    end.
-
-Ltac2 wp_power t s n :=
-    ltac1:(t |- check_status t) (Ltac1.of_constr t); waterprove t s n.
-
-
-Ltac2 new_hyp_verified_pose_proof_no_name s t u :=
-    assert $t as u by ( first [ wp_power t s 5 | print (of_string "Waterproof could not find 
-    a proof. If you believe the statement should hold, try making a smaller step") ] ).
-
-
-
-Ltac2 Notation "By" s(constr) "it" "holds" "that" t(constr) "("u(ident)")" :=
-    new_hyp_verified_pose_proof_no_name s t u.
-
-
-Goal forall n : nat, exists m : nat, n = m.
-    intro n.
-    By (n = n) it holds that (n + 1 = n + 1) (n_plus_1_eq_n_plus_1).
-Abort.
-
-
-Local Open Scope R_scope.
-
-Require Import micromega.Lra.
-
-Lemma dum : 0 < 1.
-Proof.
-    ltac1:(lra).
-Defined.
-
-
