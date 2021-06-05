00656b4729cbaff78c3c5b821fc979de3dda5e26
Lulof Pirée <l.l.piree@student.tue.nl>
2021-06-05 12:13:12 +0200

    Add testcases for [We conclude that], extend testcases [It holds that]

--- coq_tactics/new_wplib/test/test_tactics/forward_reasoning_test.v
+++ coq_tactics/new_wplib/test/test_tactics/forward_reasoning_test.v
@@ -35,26 +35,51 @@ Proof.
     ltac1:(lra).
 Qed.
 
+(* This axiom does not make sense, 
+    but therefore we can be sure that [waterprove]
+    does not know it without *explicitly* being told to use it.*)
+    Axiom zero_is_ten: 0 = 10.
+
 
 (* -------------------------------------------------------------------------- *)
 (** * Testcases for [By ... it holds that ... : ...] *)
+Ltac2 Eval (print (of_string "
+
+Testcases for [By ... it holds that ... : ...]:" )).
 
 (** * Test 1
     Base case: intoduce a sublemma with a lemma that proves it
     immediately.
+
+    NOTE: [waterprove] can apparently also find
+    [zero_lt_one] by itself.
 *)
-Lemma test1: 0 = 0.
+Lemma test_it_holds_1: 0 = 0.
 Proof.
     By zero_lt_one it holds that this_lemma:(0 < 1).
     assert_hyp_has_type @this_lemma constr:(0 < 1).
 Abort.
 
-
 (** * Test 2
+    Base case: intoduce a sublemma with a lemma that proves it
+    immediately. 
+    The sublemma cannot be proven without the given lemma!
+
+*)
+Lemma test_it_holds_2: True.
+Proof.
+    (* This is just to test if the extra lemma is really needed: *)
+    let failure () := It holds that ten_is_zero:(10 = 0) in
+    assert_raises_error failure.
+    
+    By zero_is_ten it holds that ten_is_zero:(10 = 0).
+Abort.
+
+(** * Test 3
     Corner case: try a sublemma that cannot be solved,
     at least not with the default lemmas and the provided lemma.
 *)
-Lemma test2: 0 = 0.
+Lemma test_it_holds_3: 0 = 0.
 Proof.
     let result () := 
         By zero_lt_one it holds that this_lemma:(1 > 2)
@@ -65,6 +90,10 @@ Abort.
 
 (* -------------------------------------------------------------------------- *)
 (** * Testcases for [It holds that ... : ...] *)
+Ltac2 Eval (print (of_string "
+
+Testcases for [It holds that ... : ...]:" )).
+
 
 (** * Test 1
     Base case: intoduce a sublemma that can be proven immediately.
@@ -88,6 +117,112 @@ Proof.
     assert_raises_error result.
 Abort.
 
+(* -------------------------------------------------------------------------- *)
+(** * Testcases for [We conclude that ... ] *)
+Ltac2 Eval (print (of_string "
+
+Testcases for [We conclude that ...]:" )).
+
+(** * Test 1
+    Base case: should easily be possible to finish the goal.
+*)
+Lemma test_we_conclude_1: True.
+Proof.
+    We conclude that True.
+Qed.
+
+(** * Test 2
+    Error case: wrong goal provided.
+*)
+Lemma test_we_conclude_2: True.
+Proof.
+    let result () := We conclude that False in
+    assert_raises_error result.
+Abort.
+
+(** * Test 3
+    Warning case: provided goal is equivalent, 
+        but uses an alternative notation.
+*)
+Lemma test_we_conclude_3: 2 = 2.
+Proof.
+    Ltac2 Eval (print (of_string "Should raise warning:")).
+    We conclude that (1+1 = 2).
+Qed.
+
+(** * Test 4
+    Base case, like test 1 but more complex goal.
+*)
+Lemma test_we_conclude_4: forall A: Prop, (A \/ ~A  -> True).
+Proof.
+    intros A h.
+    We conclude that (True).
+Qed.
+
+(** * Test 5
+    Error case: Waterprove cannot find proof
+    (because the statement is false!).
+*)
+Lemma test_we_conclude_5: 0 = 1.
+Proof.
+    let result () := We conclude that (0 = 1) in
+    assert_raises_error result.
+Abort.
 
 
+(* -------------------------------------------------------------------------- *)
+(** * Testcases for [By ... we conclude that ... ] *)
+Ltac2 Eval (print (of_string "
 
+Testcases for [By ... we conclude that ...]:" )).
+
+(** * Test 1
+    Base case: should easily be possible to finish the goal,
+    even with the provided lemma.
+
+    NOTE: this test would also pass if we just
+    write 
+    [We conclude that (2 = 1).]
+    Applarently, waterprove is powerful enough to apply symmetry to hypotheses.
+*)
+Lemma test_by_we_conclude_1: (1 = 2) -> (2 = 1).
+Proof.
+    intros h.
+    apply eq_sym in h. (* Rewrite h as (2 = 1) using symmetry of "="*)
+    By h we conclude that (2 = 1).
+Qed.
+
+
+(** * Test 2
+    Base case: should easily be possible to finish the goal,
+    but only with the given lemma.
+*)
+Lemma test_by_we_conclude_2: 0 + 0 = 20.
+Proof.
+    (* This is just to test if the extra lemma is really needed: *)
+    let failure () := We conclude that (0 + 0 = 10) in
+    assert_raises_error failure.
+    
+    By zero_is_ten we conclude that (0 + 0 = 20).
+Qed.
+
+(** * Test 3
+    Warning case: provided goal is equivalent, 
+        but uses an alternative notation.
+*)
+Lemma test_by_we_conclude_3: 2 = 1 + 1.
+Proof.
+    Ltac2 Eval (print (of_string "Should raise warning:")).
+    By zero_lt_one we conclude that (2 = 2).
+Qed.
+
+(** * Test 4
+    Error case: Waterprove cannot find proof
+    (because the statement is false,
+    even with the given lemma).
+*)
+Lemma test_by_we_conclude_4: 0 = 1.
+Proof.
+    let result () := By zero_is_ten we conclude that (0 = 1) in
+    assert_raises_error result.
+Abort.
