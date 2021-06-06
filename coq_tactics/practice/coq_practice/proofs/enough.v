34804549f02e140aab4cbd99a2015b3e3241d6a8
Lulof Pirée <l.l.piree@student.tue.nl>
2021-06-06 12:25:21 +0200

    Add example alternative syntax

--- coq_tactics/practice/coq_practice/proofs/enough.v
+++ coq_tactics/practice/coq_practice/proofs/enough.v
@@ -12,7 +12,7 @@ Inductive even : nat -> Prop :=
     even0 : even 0
   | evenS : forall x:nat, even x -> even (S (S x)).
 
-Lemma it_holds_example: forall x:nat, x > 1 /\ x < 3 -> even x.
+Lemma example_1: forall x:nat, x > 1 /\ x < 3 -> even x.
 Proof.
     intros x h.
     enough (x = 2).
@@ -31,3 +31,23 @@ Proof.
     exact h3.
     exact h1.
 Qed.
+
+Lemma example_2: forall x:nat, x > 1 /\ x < 3 -> even x.
+Proof.
+    intros x h.
+    enough (hyp:(x = 2)) by (
+        rewrite hyp;apply evenS;apply even0).
+
+    (* Remains to show: (x = 2) *)
+    destruct h as [h1 h3].
+    apply gt_le_S in h1. (* h1 becomes (2 <= x) *)
+    apply Nat.lt_le_pred in h3. 
+    simpl in h3. (* h3 becomes (x <= 2) *)
+
+    (* Destruct goal in (x <= 2) and (2 <= x) *)
+    apply Nat.le_antisymm.
+    exact h3.
+    exact h1.
+Qed.
+
+
