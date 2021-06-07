4f6d99cdaf33271edb83ba19dc1b31187f16c66b
Lulof Pirée <l.l.piree@student.tue.nl>
2021-06-07 16:28:05 +0200

    Cleanup implementation

--- coq_tactics/new_wplib/tactics/forward_reasoning/it_suffices_to_show.v
+++ coq_tactics/new_wplib/tactics/forward_reasoning/it_suffices_to_show.v
@@ -29,11 +29,11 @@ From Ltac2 Require Import Message.
 Add LoadPath "./coq_tactics/new_wplib/" as wplib.
 Load forward_reasoning_aux.
 
-Ltac2 fail_suffice_to_show () :=
+Local Ltac2 fail_suffice_to_show () :=
     Control.zero (AutomationFailure 
 "Waterproof could not verify that this statement is enough to prove the goal.").
 
-Ltac2 try_enough_expression f 
+Local Ltac2 try_enough_expression (f: unit -> unit) 
                            (statement: constr):=
     match Control.case f with
     | Val _ => 
@@ -61,27 +61,13 @@ Ltac2 Notation "It" "suffices" "to" "show" "that"
     statement(constr) := 
     apply_enough_with_waterprove statement.
 
-Ltac2 apply_enough_with_tactic (statement:constr) (tac:unit) :=
+Ltac2 apply_enough_with_tactic (statement:constr) (tac:unit -> unit) :=
     let hyp_name := Fresh.in_goal @h in
-    let f () := enough ($hyp_name : $statement) by tac
+    let f () := enough ($hyp_name : $statement) by (tac ())
     in
     try_enough_expression f statement.
 
 
-Ltac2 apply_enough_with_tactic_2 (statement:constr) (tac:unit -> unit) :=
-    (* If [tac] is not [unit -> unit] but just a unit,
-    then [tac] will be evaluated before [enough ($hyp_name : $statement)]
-    becomes evaluated. 
-    If [tac] would contain a tactic that uses the new statement
-    (which is usually the case when using [enough]),
-    then it would not be able to find this hypotheses.
-    Wrapping tac in a function solves this,
-    apparently it makes [tac] evaluated only later. *)
-    let hyp_name := Fresh.in_goal @h in
-    let f () := enough ($hyp_name : $statement) by (tac ())
-    in
-    f ().
-
 Ltac2 Notation "It" "suffices" "to" "show" "that" 
     statement(constr) "by" tac(thunk(tactic)) := 
-    apply_enough_with_tactic_2 statement tac.
+    apply_enough_with_tactic statement tac.
