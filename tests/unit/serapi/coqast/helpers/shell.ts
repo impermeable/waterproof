4eb0cdf0de2f9a257245f0f51f6955bd144f44d3
Cucoş, A <a.cucos@student.tue.nl>
2021-06-25 16:36:33 +0200

    atp changes

--- tests/unit/serapi/coqast/helpers/shell.ts
+++ tests/unit/serapi/coqast/helpers/shell.ts
@@ -2,7 +2,7 @@ const repl = require('repl');
 
 const ds = require('../../../../../src/coq/serapi/datastructures/index.ts');
 import {toAST, toASTWithTime} from './CoqASTHelpers';
-import {emptyAst, sexp1} from './SExprList';
+import {sexp1, veryLongSexpr} from './SExprList';
 
 function addToContext(repl, k, v, options) {
   Object.defineProperty(repl.context, k, {
@@ -21,7 +21,8 @@ addToContext(r, 'm', msg, {});
 addToContext(r, 'toAST', toAST, {});
 addToContext(r, 'toASTWithTime', toASTWithTime, {});
 addToContext(r, 'goodSExp', sexp1, {});
-addToContext(r, 'badSExp', emptyAst, {});
+addToContext(r, 'badSExp', '(CoqAST ((Bad data() () () ))', {});
+addToContext(r, 'longSexp', veryLongSexpr, {});
 
 Object.keys(ds.default).forEach((prop) => {
   addToContext(r, prop, ds.default[prop], {});
